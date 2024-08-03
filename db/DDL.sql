18ad5c8c1fd7d8195d861018b63a2c0b40722efc
DuvanCamiloArenasRodriguez <dccar21@gmail.com>
2024-08-02 23:17:17 -0500

    feat: :sparkles: English translation of the tables and their contents

--- db/DDL.sql
+++ db/DDL.sql
@@ -1,3 +1,5 @@
+-- ESPAÑOL
+
 DROP DATABASE IF EXISTS ecommerce;
 CREATE DATABASE ecommerce;
 USE ecommerce;
@@ -128,3 +130,135 @@ CREATE TABLE detalle_pedido(
     CONSTRAINT FK_pedido_producto FOREIGN KEY (id_pedido_producto) REFERENCES pedido(id)
 )ENGINE = InnoDB;
 
+--
+
+-- INGLES
+DROP DATABASE IF EXISTS ecommerce;
+CREATE DATABASE ecommerce;
+USE ecommerce;
+
+CREATE TABLE product_range (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description TEXT NULL,
+    image VARCHAR(256) NULL,
+    CONSTRAINT PK_product_range PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE state (
+    id INT AUTO_INCREMENT NOT NULL,
+    state_name VARCHAR(20) NOT NULL,
+    CONSTRAINT PK_state PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE payment_method (
+    id INT AUTO_INCREMENT NOT NULL,
+    method_name VARCHAR(15),
+    CONSTRAINT PK_payment_method PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE city (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    CONSTRAINT PK_city PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE address (
+    id INT AUTO_INCREMENT NOT NULL,
+    address_line1 VARCHAR(50) NOT NULL,
+    address_line2 VARCHAR(50) NULL,
+    city_code_d INT(10) NOT NULL,
+    CONSTRAINT PK_id PRIMARY KEY (id),
+    CONSTRAINT FK_address_city FOREIGN KEY (city_code_d) REFERENCES city(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE office (
+    id INT AUTO_INCREMENT NOT NULL,
+    address_code_o INT(10) NOT NULL,
+    CONSTRAINT PK_office PRIMARY KEY (id),
+    CONSTRAINT FK_office_address FOREIGN KEY (address_code_o) REFERENCES address(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name1 VARCHAR(50) NOT NULL,
+    last_name2 VARCHAR(50) NULL,
+    email VARCHAR(100) NOT NULL,
+    office_code INT(10) NULL,
+    CONSTRAINT PK_employee PRIMARY KEY (id),
+    CONSTRAINT FK_employee_office FOREIGN KEY (office_code) REFERENCES office(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE customer (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name1 VARCHAR(30),
+    last_name2 VARCHAR(30),
+    email VARCHAR(50),
+    city_code_c INT(10) NOT NULL,
+    address_code_c INT(10) NOT NULL,
+    sales_rep_employee_code INT(10) NULL,
+    CONSTRAINT PK_customer PRIMARY KEY (id),
+    CONSTRAINT FK_customer_city FOREIGN KEY (city_code_c) REFERENCES city(id),
+    CONSTRAINT FK_customer_address FOREIGN KEY (address_code_c) REFERENCES address(id),
+    CONSTRAINT FK_customer_sales_rep FOREIGN KEY (sales_rep_employee_code) REFERENCES employee(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE phone (
+    id INT AUTO_INCREMENT NOT NULL,
+    number BIGINT NOT NULL,
+    customer_code_ph INT(10) NULL,
+    office_code_ph INT(10) NULL,
+    CONSTRAINT PK_phone PRIMARY KEY (id),
+    CONSTRAINT FK_phone_customer FOREIGN KEY (customer_code_ph) REFERENCES customer(id),
+CONSTRAINT FK_phone_office FOREIGN KEY (office_code_ph) REFERENCES office(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE payment (
+    id INT AUTO_INCREMENT NOT NULL,
+    payment_date DATE NOT NULL,
+    total DOUBLE NOT NULL,
+    payment_method_code INT(10) NOT NULL,
+    customer_code_pa INT(10) NOT NULL,
+    CONSTRAINT PK_payment PRIMARY KEY (id),
+    CONSTRAINT FK_payment_method FOREIGN KEY (payment_method_code) REFERENCES payment_method(id),
+    CONSTRAINT FK_payment_customer FOREIGN KEY (customer_code_pa) REFERENCES customer(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE order (
+    id INT AUTO_INCREMENT NOT NULL,
+    order_date DATE NOT NULL,
+    expected_date DATE NOT NULL,
+    delivery_date DATE NULL,
+    comment TEXT NULL,
+    customer_code_or INT(10) NOT NULL,
+    status_code_or INT(10) NOT NULL,
+    CONSTRAINT PK_order PRIMARY KEY (id),
+    CONSTRAINT FK_order_customer FOREIGN KEY (customer_code_or) REFERENCES customer(id),
+    CONSTRAINT FK_order_status FOREIGN KEY (status_code_or) REFERENCES status(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE product (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(70) NOT NULL,
+    stock_quantity TINYINT NOT NULL,
+    sale_price DOUBLE NOT NULL,
+    supplier_price DOUBLE NOT NULL,
+    product_description TEXT NULL,
+    range_code INT(10) NOT NULL,
+    dimensions VARCHAR(255) NULL,
+    CONSTRAINT PK_product PRIMARY KEY (id),
+    CONSTRAINT FK_product_range FOREIGN KEY (range_code) REFERENCES product_range(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE order_detail (
+    product_id_order INT NOT NULL,
+    order_id_product INT NOT NULL,
+    quantity SMALLINT NOT NULL,
+    unit_price DOUBLE NULL,
+    line_number SMALLINT NULL,
+    PRIMARY KEY (product_id, order_id),
+    CONSTRAINT FK_product_order FOREIGN KEY (product_id_order) REFERENCES product(id),
+CONSTRAINT FK_order_product FOREIGN KEY (order_id_product) REFERENCES order(id)
+) ENGINE = InnoDB;
