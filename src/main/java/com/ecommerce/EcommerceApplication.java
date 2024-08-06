b3f26540ed35fd0bf17800f7c72fced0a51e85e3
Michael-Chacon <michaelchacon02@gmail.com>
2024-08-06 06:55:36 -0500

    feat: :sparkles: Corrigiendo error de CORS

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -20,8 +20,8 @@ public class EcommerceApplication{
             @SuppressWarnings("null")
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/").allowedOrigins("http://127.0.0.1:5500/",
-                        "http://localhost:8080").allowedMethods("").allowedHeaders("");
+                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500",
+                        "http://localhost:8080").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
